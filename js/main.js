13d30b53a30f2ee9fa2d9c168579e36d451b8826
Simone Cioffi <56473031+SimoneCff@users.noreply.github.com>
2021-11-23 19:56:34 +0100

    -1 Added js folder -2 Added main.js -3 Added service-worker.js

---
+++ js/main.js
@@ -0,0 +1,17 @@
+window.onload = () => {
+    'use strict';
+
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker
+            .register('./service-worker.js').then(function (registration) {
+
+            // Service worker registered correctly.
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        },
+            function (err) {
+
+                // Troubles in registering the service worker. :(
+                console.log('ServiceWorker registration failed: ', err);
+            });
+    }
+}
