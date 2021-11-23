90b25128b6a0e86053c1aac7b1c635d46809808a
Simone Cioffi <56473031+SimoneCff@users.noreply.github.com>
2021-11-23 20:35:17 +0100

    -1 modified service worker

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,13 @@
 let cacheName = 'papc';
 let filesToCache = [
-    'index.html',
     'manifest.json',
+    'index.html',
     'css/style.css',
     'js/main.js',
     'images/cpu.png',
     'images/mobo.png',
     'images/psu.png',
-    'images/ram.png',
-    'images/icon.png'
+    'images/ram.png'
 ];
 /* Start the service worker and cache all of the app's content */
 console.log('Service worker online');
