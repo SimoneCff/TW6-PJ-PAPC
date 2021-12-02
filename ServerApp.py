7485722d52a7953900477b11e3ff1a85859ad7bf
Simone Cioffi <56473031+SimoneCff@users.noreply.github.com>
2021-12-02 14:19:00 +0100

    -1 modified Flask app

--- ServerApp.py
+++ ServerApp.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-if __name__=='__name__':
+
+if __name__ == '__name__':
     app.run(debug=True)
