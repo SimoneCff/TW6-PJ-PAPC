1c177ecc76a758f372d16eb5a01688147bb2216f
Simone Cioffi <56473031+SimoneCff@users.noreply.github.com>
2021-12-10 20:14:30 +0100

    -1 added linking to MongoDB atlas DataBase

--- ServerApp.py
+++ ServerApp.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
 
 app = Flask(__name__, template_folder='', static_folder='')
 
@@ -10,3 +11,7 @@ def index():
 
 if __name__ == '__name__':
     app.run(debug=True)
+
+client = MongoClient("mongodb+srv://admin:admin@papc.q16vr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = client.test
+
