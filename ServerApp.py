ba6d89520b725bb4140d0000acf44321f7154c7e
Simone Cioffi <56473031+SimoneCff@users.noreply.github.com>
2021-12-11 15:09:29 +0100

    -1 create extensions.py for extends the app.py -2 included extension.py inside serverapp

--- ServerApp.py
+++ ServerApp.py
@@ -1,17 +1,17 @@
 from flask import Flask, render_template
-from pymongo import MongoClient
+from py import extensions
 
 app = Flask(__name__, template_folder='', static_folder='')
-
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-
 if __name__ == '__name__':
     app.run(debug=True)
 
-client = MongoClient("mongodb+srv://admin:admin@papc.q16vr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-db = client.test
+
+
+
+
 
---
+++ py/extensions.py
@@ -0,0 +1,11 @@
+from flask_pymongo import MongoClient
+
+client = MongoClient("mongodb+srv://admin:admin@papc.q16vr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+
+db = client["PAPC"]
+cpu = db["CPU"]
+
+
+
+for x in cpu.find():
+    print(x)
