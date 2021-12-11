84124de8f473a767562f99a0673ea99d4e3fd876
Simone Cioffi <56473031+SimoneCff@users.noreply.github.com>
2021-12-11 18:25:40 +0100

    -1 modified all the sub-html files for loading files in flask -2 modified app.py for render the other html files into the iframe on index -3 modified cpu.html for an error

--- app.py
+++ app.py
@@ -4,11 +4,40 @@ app = Flask(__name__, static_url_path='', template_folder='templates', static_fo
 
 @app.route('/')
 def index():
-    return render_template("index.html");
+    return render_template("index.html")
+
+@app.route('/cpu')
+def cpu():
+    return render_template("cpu.html")
+
+@app.route('/case')
+def case():
+    return render_template("case.html")
+
+@app.route('/cooling')
+def cooling():
+    return render_template("cooling.html")
+
+@app.route('/memory')
+def memory():
+    return render_template("memory.html")
+
+@app.route('/mobo')
+def mobo():
+    return render_template("mobo.html")
+
+@app.route('/psu')
+def psu():
+    return render_template("psu.html")
+
+@app.route('/ram')
+def ram():
+    return render_template("ram.html")
 
 @app.route('/service-worker.js')
 def sw():
     return app.send_static_file('service-worker.js')
+
 
 if __name__ == '__name__':
     app.run(debug=True)
