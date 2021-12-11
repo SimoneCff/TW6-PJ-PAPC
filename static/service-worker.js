3db48c752afc63c12ecaf1d18e36f0b568368d08
Simone Cioffi <56473031+SimoneCff@users.noreply.github.com>
2021-12-11 15:47:59 +0100

    -1 modified index for integrate it with flask -2 modified service-worker.js

--- static/service-worker.js
+++ static/service-worker.js
@@ -23,6 +23,7 @@ self.addEventListener('install', function(event) {
         })
     );
 });
+
 self.addEventListener('fetch', function(event) {
     event.respondWith(
         caches.match(event.request)
