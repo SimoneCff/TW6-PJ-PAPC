9c689061bec3838fc78e7902438300099d27e6df
Simone Cioffi <56473031+SimoneCff@users.noreply.github.com>
2021-12-14 21:17:29 +0100

    -modifies for testing with LIGHTHOUSE

--- static/js/main.js
+++ static/js/main.js
@@ -3,7 +3,7 @@ window.onload = () => {
 
 // service-worker:
     if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('/sw.js').then(function (registration) {
+            navigator.serviceWorker.register('./sw.js').then(function (registration) {
                 console.log('Service Worker registration was successful with scope: ', registration.scope);
             }).catch(function (err) {
                 console.log('ServiceWorker registration failed: ', err);
