0d51649edaaef922795129c601da8c0ebf38145f
simonecioffi <simone.cioffi001@studenti.uniparthenope.it>
2021-12-16 11:27:03 +0100

    1. modified app.py for a problem

--- app.py
+++ app.py
@@ -7,23 +7,32 @@ from bson.json_util import dumps
 
 app = Flask(__name__, static_url_path='', template_folder='templates', static_folder='static')
 app.config.from_object(Config)
+app.config['CACHE_TYPE'] = 'simple'
 
 @app.route('/')
+
 def index():
     return render_template("index.html")
 
-@app.route('/cpu',  methods=['GET', 'POST'])
+
+@app.route('/cpu', methods=['POST', 'GET'])
 def cpu():
     form1 = Searchfor()
     quer = list()
 
     if form1.validate_on_submit():
         query = SearchIntoDb("CPU", request.form.get('search')).findquery()
+
         for x in query:
             quer.insert(1,[dumps(x['name']), dumps(x['marca']), dumps(x['COSTO']), dumps(x['_id'])])
         return render_template("cpu.html", form=form1, queri=quer)
 
+    if request.method == 'POST':
+        if request.form['Compra'] == 'Compra':
+            print(request.form.get['val'])
+
     return render_template("cpu.html", form=form1)
+
 
 
 @app.route('/case')
