e7b6018728e76fad594c3ae6a675b5f518b449da
simonecioffi <simone.cioffi001@studenti.uniparthenope.it>
2021-12-22 16:10:34 +0100

    Final Commit

--- routes/checkout.py
+++ routes/checkout.py
@@ -5,9 +5,14 @@ from app import Carrello, app
 
 @app.route('/checkout', methods=['POST', 'GET'])
 def checkout():
+    right = False
     if request.method == 'POST':
         if request.form.get('remove'):
             x = request.form.get('remove')
             Carrello.Remove(int(x))
-
-    return render_template("checkout.html", trolley=Carrello.returnList())
+        else:
+            right = True
+            return render_template("checkout.html", trolley=Carrello.returnList(), right=right, psu=Carrello.SeeWatt(),
+                                   case=Carrello.SeeCompatibiltyCase(), ram=Carrello.SeeCompatibiltyRAM(),
+                                   cpu=Carrello.SeeCompatibiltyCPU())
+    return render_template("checkout.html", trolley=Carrello.returnList(), right=right)
