ca25d91cbb3d8aac4dd0849d38ef3d77d7195c98
Simone Cioffi <56473031+SimoneCff@users.noreply.github.com>
2021-12-22 12:21:01 +0100

    1. added gpu

--- routes/gpu.py
+++ routes/gpu.py
@@ -1,11 +1,7 @@
-from bson.json_util import dumps
 from flask import request, render_template
 
 from app import Carrello, app
-from complements.db import SearchIntoDb, SearchviaAttributesCASE
-from complements.forms import Searchfor, CaseSelect
-
 
 @app.route('/gpu', methods=['POST', 'GET'])
 def gpu():
-    render_template("gpu.html")
+    return render_template("gpu.html")
