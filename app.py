d3604faf32eccfdea2b833f8a3c3f5b80c63c00f
Simone Cioffi <56473031+SimoneCff@users.noreply.github.com>
2021-12-22 12:12:55 +0100

    1. added gpu

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__, static_url_path='', template_folder='templates', static_fo
 app.config.from_object(Config)
 Carrello = Trolley()
 
-import routes.case, routes.checkout, routes.cooling, routes.cpu, routes.idex, routes.memory, routes.mobo, routes.psu, routes.ram
+import routes.case, routes.checkout, routes.cooling, routes.cpu, routes.idex, routes.memory, routes.mobo, routes.psu, routes.ram, routes.gpu
 
 if __name__ == '__name__':
     app.run(debug=True, ssl_contex='adhoc')
